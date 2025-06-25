767e5a9d7849dd1efd2c23ab0dc14e706e9893de
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-06-25 20:13:17 +0545

    Update Program.cs to test CRUD functions

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
 ﻿using System;
-Console.WriteLine("Hello, World!");
+
+public class Program {
+	public static void Main(string[] args) {
+		StatsController statsController = new StatsController();
+
+		Console.WriteLine(statsController.ReadAll());
+
+		statsController.Create(
+			id: 5, 
+			coins: 189, 
+			level: 34, 
+			xp: 128, 
+			playerId: "7341c974-3e92-429c-b15d-359e3d1a4e06"
+		);
+		Console.WriteLine(statsController.ReadAll());
+		
+		Console.WriteLine(statsController.ReadById(id: 2));
+		statsController.Update(
+			id:2,
+			coins: 0,
+			level: 1,
+			xp: 0,
+			playerId: "9afacf79-5720-4a60-9a8c-7d9509433a4b"
+		);
+		Console.WriteLine(statsController.ReadById(id: 2));
+
+		statsController.DeleteById(id: 2);
+		Console.WriteLine(statsController.ReadAll());
+	}
+}
