f8c01a8e054bd56bdf95447c3923acacb65b1b3f
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-07-03 00:03:40 +0545

    Update Program.cs to setup basic ASP.NET Core Web API | Week 2

--- Program.cs
+++ Program.cs
@@ -1,29 +1,13 @@
-﻿public class Program {
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+public class Program {
 	public static void Main(string[] args) {
-		StatService statsService = new();
-		StatController statsController = new(statsService);
+		WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+		builder.Services.AddControllers();
 
-		statsController.ReadAll();
-
-		statsController.Create(
-			coins: 189, 
-			level: 34, 
-			xp: 128, 
-			playerId: "7341c974-3e92-429c-b15d-359e3d1a4e06"
-		);
-		statsController.ReadAll();
-		
-		statsController.ReadById(id: 2);
-		statsController.Update(
-			id:2,
-			coins: 0,
-			level: 1,
-			xp: 0,
-			playerId: "9afacf79-5720-4a60-9a8c-7d9509433a4b"
-		);
-		statsController.ReadById(id: 2);
-
-		statsController.Delete(id: 2);
-		statsController.ReadAll();
+		WebApplication app = builder.Build();
+		app.MapControllers();
+		app.Run();
 	}
 }
