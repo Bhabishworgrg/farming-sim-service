1bd14ee769d9c93d6829a86539998da49262085c
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-07-07 23:57:58 +0545

    Create Stat model | Week 2

---
+++ Models/Stat.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class Stat {
+	public int Id { get; set; }
+	public int Coins { get; set; }
+	public short Level { get; set; }
+	public int Xp { get; set; }
+	public Guid PlayerId { get; set; } 
+}
