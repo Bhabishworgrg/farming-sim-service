4f07a343958990caa13e0eaf612fc6c1cfbfbf20
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-07-14 21:26:39 +0545

    Update StatService to implement IStatService | Week 3

--- Services/StatService.cs
+++ Services/StatService.cs
@@ -1,94 +1,99 @@
 using System.Collections.Generic;
+using System.Linq;
 
-public class StatService {
-	private static List<Stat> stats = new() {
-		new() {Id=1, Coins=100, Level=25, Xp=3899},
-		new() {Id=2, Coins=930, Level=51, Xp=3438124},
-		new() {Id=3, Coins=645, Level=29, Xp=15874},
-		new() {Id=4, Coins=331, Level=37, Xp=32801},
-	};
-	
-	public StatResponseDto CreateStat(StatRequestDto request) {
-		int id = stats[^1].Id + 1;
+public class StatService : IStatService {
+	private IBaseRepository _repository;
+
+	public StatService(IBaseRepository repository) {
+		_repository = repository;
+	}
+
+    public DataResult<StatResponseDto> Create(StatRequestDto requestDto) {
 		Stat stat = new() {
-			Id=id, 
-			Coins=request.Coins, 
-			Level=request.Level, 
-			Xp=request.Xp,
+			Xp=requestDto.Xp,
+			Coins=requestDto.Coins,
+			Level=requestDto.Level
 		};
 
-		stats.Add(stat);
-		
-		return new StatResponseDto {
+		DataResult<Stat> result = _repository.Create(stat);
+
+		StatResponseDto? responseDto = (result.Model == null) ? null : new() {
+			Id=result.Model.Id,
+			Xp=result.Model.Xp,
+			Coins=result.Model.Coins,
+			Level=result.Model.Level
+		};
+
+		return new DataResult<StatResponseDto> {
+			Model=responseDto, 
+			StatusCode=result.StatusCode,
+			Message=result.Message
+		};
+    }
+
+    public DataResult<StatResponseDto> Read(int id) {
+		DataResult<Stat> result = _repository.Read<Stat>(id);
+		StatResponseDto? responseDto = (result.Model == null) ? null : new() {
+			Id=result.Model.Id,
+			Xp=result.Model.Xp,
+			Coins=result.Model.Coins,
+			Level=result.Model.Level
+		};
+
+		return new DataResult<StatResponseDto> {
+			Model=responseDto,
+			StatusCode=result.StatusCode,
+			Message=result.Message
+		};
+    }
+
+    public DataResult<List<StatResponseDto>> ReadAll() {
+		DataResult<List<Stat>> result = _repository.ReadAll<Stat>();
+
+		List<StatResponseDto> responseDtos = result.Model!.Select(stat => new StatResponseDto {
 			Id=stat.Id,
-			Coins=stat.Coins, 
-			Level=stat.Level, 
-			Xp=stat.Xp, 
-		};
-	}
-	
-	public StatResponseDto? ReadStat(int id) {
-		foreach (Stat stat in stats) {
-			if (stat.Id == id) {
-				return new StatResponseDto {
-					Id=stat.Id,
-					Coins=stat.Coins,
-					Level=stat.Level,
-					Xp=stat.Xp,
-				};
-			}
-		}
-		return null;
-	}
-	
-	public List<StatResponseDto> ReadStats() {
-		List<StatResponseDto> result = new();
-		foreach (Stat stat in stats) {
-			result.Add(new StatResponseDto {
-				Id=stat.Id,
-				Coins=stat.Coins,
-				Level=stat.Level,
-				Xp=stat.Xp,
-			});
-		}
-		return result;
-	}
-	
-	public StatResponseDto? UpdateStat(int id, StatRequestDto request) {
-		for (int i = 0; i < stats.Count; i++) {
-			if (stats[i].Id == id) {
-				Stat stat = new() {
-					Id=id,
-					Coins=request.Coins,
-					Level=request.Level,
-					Xp=request.Xp,
-				};
-				
-				stats[i] = stat;
+			Xp=stat.Xp,
+			Coins=stat.Coins,
+			Level=stat.Level
+		}).ToList();
 
-				return new StatResponseDto {
-					Id=stat.Id,
-					Coins=stat.Coins,
-					Level=stat.Level,
-					Xp=stat.Xp,
-				};
-			}
-		}
-		return null;
-	}
-	
-	public StatResponseDto? DeleteStat(int id) {
-		foreach (Stat stat in stats) {
-			if (stat.Id == id) {
-				stats.Remove(stat);
-				return new StatResponseDto {
-					Id=stat.Id,
-					Coins=stat.Coins,
-					Level=stat.Level,
-					Xp=stat.Xp,
-				};
-			}
-		}
-		return null;
-	}
+		return new DataResult<List<StatResponseDto>> {
+			Model=responseDtos,
+			StatusCode=result.StatusCode,
+			Message=result.Message
+		};
+    }
+
+    public DataResult<StatResponseDto> Update(int id, StatRequestDto requestDto) {
+		Stat stat = new() {
+			Id=id,
+			Xp=requestDto.Xp,
+			Coins=requestDto.Coins,
+			Level=requestDto.Level
+		};
+
+		DataResult<Stat> result = _repository.Update(stat);
+
+		StatResponseDto? responseDto = (result.Model == null) ? null : new() {
+			Id=result.Model.Id,
+			Xp=result.Model.Xp,
+			Coins=result.Model.Coins,
+			Level=result.Model.Level
+		};
+
+		return new DataResult<StatResponseDto> {
+			Model=responseDto,
+			StatusCode=result.StatusCode,
+			Message=result.Message
+		};
+    }
+
+    public DataResult<StatResponseDto> Delete(int id) {
+		DataResult<Stat> result = _repository.Delete<Stat>(id);
+
+		return new DataResult<StatResponseDto> {
+			StatusCode=result.StatusCode,
+			Message=result.Message
+		};
+    }
 }
