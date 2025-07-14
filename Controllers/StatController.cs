60eec92759d5f1974e64c63d51a32fe4fff1b513
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-07-14 21:27:02 +0545

    Update StatController to use updated StatService | Week 3

--- Controllers/StatController.cs
+++ Controllers/StatController.cs
@@ -4,51 +4,34 @@ using System.Collections.Generic;
 [ApiController]
 [Route("api/stats")]
 public class StatController : ControllerBase {
-	private StatService service = new();
+	private IStatService _service;
+
+	public StatController(IStatService service) {
+		_service = service;
+	}
 
 	[HttpPost]
-	public DataResult<StatResponseDto> CreateStat(StatRequestDto request) {
-		StatResponseDto response = service.CreateStat(request);
-		return new() {
-			Model=response,
-			StatusCode=201,
-			Message=$"Stat #{response.Id} created successfully."
-		};
+	public DataResult<StatResponseDto> Create(StatRequestDto requestDto) {
+		return _service.Create(requestDto);
 	}
 
 	[HttpGet("{id}")]
-	public DataResult<StatResponseDto> ReadStat(int id) {
-		return new() {
-			Model=service.ReadStat(id),
-			StatusCode=200,
-			Message=$"Stat #{id} retrieved successfully."
-		};
+	public DataResult<StatResponseDto> Read(int id) {
+		return _service.Read(id);
 	}
 
 	[HttpGet]
-	public DataResult<List<StatResponseDto>> ReadStats() {
-		return new() {
-			Model=service.ReadStats(),
-			StatusCode=200,
-			Message="Stats retrieved successfully."
-		};
+	public DataResult<List<StatResponseDto>> ReadAll() {
+		return _service.ReadAll(); 
 	}
 
 	[HttpPut("{id}")]
-	public DataResult<StatResponseDto> UpdateStat(int id, StatRequestDto request) {
-		return new() {
-			Model=service.UpdateStat(id, request),
-			StatusCode=200,
-			Message=$"Stat #{id} updated successfully."
-		};
+	public DataResult<StatResponseDto> Update(int id, StatRequestDto requestDto) {
+		return _service.Update(id, requestDto);
 	}
 
 	[HttpDelete("{id}")]
 	public DataResult<StatResponseDto> Delete(int id) {
-		return new() {
-			Model=service.DeleteStat(id),
-			StatusCode=200,
-			Message=$"Stat #{id} deleted successfully."
-		};
+		return _service.Delete(id);
 	}
 }
