444fbd325ac15c06d7ff0e63cb2e79d75a3324ce
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-07-15 13:33:05 +0545

    Add PlayerId FK in Stats | Week 3

---
+++ Migrations/20250715074027_Add_ForeignKey_In_Stat.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FarmingSimService.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_ForeignKey_In_Stat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PlayerId",
+                table: "Stats",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stats_PlayerId",
+                table: "Stats",
+                column: "PlayerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Stats_Players_PlayerId",
+                table: "Stats",
+                column: "PlayerId",
+                principalTable: "Players",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Stats_Players_PlayerId",
+                table: "Stats");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Stats_PlayerId",
+                table: "Stats");
+
+            migrationBuilder.DropColumn(
+                name: "PlayerId",
+                table: "Stats");
+        }
+    }
+}
--- Models/Stat.cs
+++ Models/Stat.cs
@@ -2,4 +2,6 @@ public class Stat : BaseEntity {
 	public int Coins { get; set; }
 	public short Level { get; set; }
 	public int Xp { get; set; }
+	public int PlayerId { get; set; }
+	public Player Player { get; set; } = null!;
 }
