d97dc97a61d1367a5e5ebca73dea2bab9a237795
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-07-22 00:33:24 +0545

    Create DTOs for registration | Week 4

---
+++ DTOs/RegisterResponseDto.cs
@@ -0,0 +1,5 @@
+public class RegisterResponseDto {
+	public string Id { get; set; } = string.Empty;
+	public string UserName { get; set; } = string.Empty;
+	public string Email { get; set; } = string.Empty;
+}
