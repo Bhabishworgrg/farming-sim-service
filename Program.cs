0d463bfee999518b20bffcf419eae462c772c497
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-07-21 20:33:32 +0545

    Setup basic authentication and authorization | Week 4

--- Program.cs
+++ Program.cs
@@ -2,17 +2,29 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Configuration;
+using Microsoft.AspNetCore.Identity;
 
 WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers();
-builder.Services.AddDbContext<AppDbContext>(options =>
-	options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(
+	options => options.UseNpgsql(connectionString)
 );
+
+builder.Services.AddIdentityCore<IdentityUser>()
+	.AddEntityFrameworkStores<AppDbContext>();
+
+builder.Services.AddAuthentication();
+builder.Services.AddAuthorization();
+
+builder.Services.AddControllers();
+
 builder.Services.AddScoped<IBaseRepository, BaseRepository>();
 builder.Services.AddScoped<IStatService, StatService>();
 builder.Services.AddScoped<IPlayerService, PlayerService>();
 
 WebApplication app = builder.Build();
+app.UseAuthentication();
+app.UseAuthorization();
 app.MapControllers();
 app.Run();
