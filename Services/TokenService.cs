b9740a743a00f9ad56f5961745b1d67cce2c206a
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-07-27 20:17:11 +0545

    Create TokenService for generating token | Week 5

---
+++ Services/TokenService.cs
@@ -0,0 +1,40 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+public class TokenService : ITokenService
+{
+	private IConfiguration _configuration;
+
+	public TokenService(IConfiguration configuration) {
+		_configuration = configuration;
+	}
+
+    public string GenerateToken(IdentityUser user)
+    {
+		Claim[] claims = new[] {
+			new Claim(JwtRegisteredClaimNames.Sub, user.Id),
+			new Claim(JwtRegisteredClaimNames.Name, user.UserName!),
+			new Claim(JwtRegisteredClaimNames.Email, user.Email!)
+		};
+
+		SymmetricSecurityKey key = new(
+			Encoding.UTF8.GetBytes(_configuration["Api:SecretKey"]!)
+		);
+		SigningCredentials credentials = new(key, SecurityAlgorithms.HmacSha256);
+
+		JwtSecurityToken token = new(
+			issuer: _configuration["Api:Issuer"],
+			audience: _configuration["Api:Audience"],
+			claims: claims,
+			expires: DateTime.Now.AddDays(1),
+			signingCredentials: credentials
+		);
+
+		return new JwtSecurityTokenHandler().WriteToken(token);
+    }
+}
