20575dfd97328383d95300a9915ed3e0bc40677b
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-07-29 16:08:48 +0545

    Add migrations for Storage, Patches, Buildings | Week 6

---
+++ Data/Migrations/20250729084509_Add_Remaining_Entities.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FarmingSimService.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Remaining_Entities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Buildings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Type = table.Column<string>(type: "text", nullable: false),
+                    Level = table.Column<short>(type: "smallint", nullable: false),
+                    X = table.Column<short>(type: "smallint", nullable: false),
+                    Y = table.Column<short>(type: "smallint", nullable: false),
+                    PlayerId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Buildings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Buildings_Players_PlayerId",
+                        column: x => x.PlayerId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Patches",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Stage = table.Column<int>(type: "integer", nullable: false),
+                    PlantedTime = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    X = table.Column<short>(type: "smallint", nullable: false),
+                    Y = table.Column<short>(type: "smallint", nullable: false),
+                    PlayerId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Patches", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Patches_Players_PlayerId",
+                        column: x => x.PlayerId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Storage",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Quantity = table.Column<short>(type: "smallint", nullable: false),
+                    PlayerId = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Storage", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Storage_Players_PlayerId",
+                        column: x => x.PlayerId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Buildings_PlayerId",
+                table: "Buildings",
+                column: "PlayerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Patches_PlayerId",
+                table: "Patches",
+                column: "PlayerId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Storage_PlayerId",
+                table: "Storage",
+                column: "PlayerId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Buildings");
+
+            migrationBuilder.DropTable(
+                name: "Patches");
+
+            migrationBuilder.DropTable(
+                name: "Storage");
+        }
+    }
+}
