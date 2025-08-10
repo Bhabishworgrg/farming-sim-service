326fedf14196b08c7a61a04855285185801add32
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-08-11 01:08:08 +0545

    Run migration | Week 7

---
+++ Data/Migrations/20250810191814_Combine Stat in Player.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace FarmingSimService.Migrations
+{
+    /// <inheritdoc />
+    public partial class CombineStatinPlayer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stats");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Coins",
+                table: "Players",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<short>(
+                name: "Level",
+                table: "Players",
+                type: "smallint",
+                nullable: false,
+                defaultValue: (short)0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Xp",
+                table: "Players",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Coins",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Level",
+                table: "Players");
+
+            migrationBuilder.DropColumn(
+                name: "Xp",
+                table: "Players");
+
+            migrationBuilder.CreateTable(
+                name: "Stats",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    PlayerId = table.Column<int>(type: "integer", nullable: false),
+                    Coins = table.Column<int>(type: "integer", nullable: false),
+                    Level = table.Column<short>(type: "smallint", nullable: false),
+                    Xp = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stats", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Stats_Players_PlayerId",
+                        column: x => x.PlayerId,
+                        principalTable: "Players",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Stats_PlayerId",
+                table: "Stats",
+                column: "PlayerId");
+        }
+    }
+}
