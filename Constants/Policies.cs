45d0f15e89e0da0068b9aaf7e78ad03ee1a8cb4d
Bhabishworgrg <bhabishworgrg@gmail.com>
2025-08-18 14:58:22 +0545

    Create Policy constants | Week 8

---
+++ Constants/Policies.cs
@@ -0,0 +1,7 @@
+public static class Policies {
+	public static class Owner {
+		public const string Building = "BuildingOwner";
+		public const string Patch = "PatchOwner";
+		public const string Storage = "StorageOwner";
+	}
+}
